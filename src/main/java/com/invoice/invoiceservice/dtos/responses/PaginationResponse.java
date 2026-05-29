120d014e0bddd6248ca5903f5b939ea4f60126f2
Murilo R Vargas <muriraubach@gmail.com>
2026-05-29 20:33:23 -0300

    feat(dtos): implement generic pagination response record

--- src/main/java/com/invoice/invoiceservice/dtos/responses/PaginationResponse.java
+++ src/main/java/com/invoice/invoiceservice/dtos/responses/PaginationResponse.java
@@ -1,4 +1,12 @@
 package com.invoice.invoiceservice.dtos.responses;
 
-public class PaginationResponse {
+import java.util.List;
+
+public record PaginationResponse<T>(List<T> data, Pagination pagination) {
+
+    public record Pagination(int page, int pageSize) {}
+
+    public static <T> PaginationResponse<T> of(List<T> data, int page, int pageSize) {
+        return new PaginationResponse<>(data, new Pagination(page, pageSize));
+    }
 }
