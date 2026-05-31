92ae6eb7b83d13c0c89544b9e8c28e1a257f28c4
Murilo R Vargas <muriraubach@gmail.com>
2026-05-31 17:57:34 -0300

    feat(invoice): add JpaSpecificationExecutor to InvoiceRepository and findAllByInvoice to InvoiceItemRepository

--- src/main/java/com/invoice/invoiceservice/repositories/InvoiceItemRepository.java
+++ src/main/java/com/invoice/invoiceservice/repositories/InvoiceItemRepository.java
@@ -1,7 +1,12 @@
 package com.invoice.invoiceservice.repositories;
 
+import com.invoice.invoiceservice.entities.Invoice;
 import com.invoice.invoiceservice.entities.InvoiceItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InvoiceItemRepository extends JpaRepository<InvoiceItem, Long> {
+
+    List<InvoiceItem> findAllByInvoice(Invoice invoice);
 }
--- src/main/java/com/invoice/invoiceservice/repositories/InvoiceRepository.java
+++ src/main/java/com/invoice/invoiceservice/repositories/InvoiceRepository.java
@@ -3,11 +3,12 @@ package com.invoice.invoiceservice.repositories;
 import com.invoice.invoiceservice.entities.Invoice;
 import com.invoice.invoiceservice.entities.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+public interface InvoiceRepository extends JpaRepository<Invoice, Long>, JpaSpecificationExecutor<Invoice> {
 
     Optional<Invoice> findByWalletAndClosingDate(Wallet wallet, LocalDate closingDate);
 }
